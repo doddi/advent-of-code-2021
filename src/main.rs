27920b5764c27532e055c8e0af1a979972fe6bd0
mdodgson <mdodgson@sonatype.com>
2021-12-02 08:52:49 +0000

    day2 - part2

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     let tokens = split_lines(lines);
 
-    let position = calculate_position(&tokens);
+    let position = calculate_aim(&tokens);
 
     println!("{}", position.0 * position.1)
 }
@@ -22,6 +22,30 @@ fn calculate_position(coords : &Vec<(String, i32)>) -> (i32, i32) {
             ("up", step) => {y_pos = y_pos - step}
             ("down", step) => {y_pos = y_pos + step}
             ("forward", step) => {x_pos = x_pos + step}
+            ("backward", step) => {x_pos = x_pos - step}
+            _ => {
+                println!("Error");
+                exit(1)
+            }
+        }
+    }
+
+    return (x_pos, y_pos)
+}
+
+fn calculate_aim(coords : &Vec<(String, i32)>) -> (i32, i32) {
+    let mut aim = 0;
+    let mut x_pos = 0;
+    let mut y_pos = 0;
+
+    for coord in coords {
+        match (coord.0.as_str(), coord.1) {
+            ("up", step) => {aim = aim - step}
+            ("down", step) => {aim = aim + step}
+            ("forward", step) => {
+                x_pos = x_pos + step;
+                y_pos = y_pos + (aim * step)
+            }
             ("backward", step) => {x_pos = x_pos - step}
             _ => {
                 println!("Error");
@@ -73,5 +97,23 @@ mod test {
         assert_eq!(result.0, 15);
         assert_eq!(result.1, 10);
         assert_eq!(mult, 150);
+    }
+
+    #[test]
+    fn test_calculate_aim() {
+        let coords = vec![
+            ("forward".to_ascii_lowercase(), 5),
+            ("down".to_ascii_lowercase(), 5),
+            ("forward".to_ascii_lowercase(), 8),
+            ("up".to_ascii_lowercase(), 3),
+            ("down".to_ascii_lowercase(), 8),
+            ("forward".to_ascii_lowercase(), 2),
+        ];
+
+        let result = calculate_aim(&coords);
+        let mult = result.0 * result.1;
+        assert_eq!(result.0, 15);
+        assert_eq!(result.1, 60);
+        assert_eq!(mult, 900);
     }
 }
